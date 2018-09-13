96ff74f12bd01e661e2d8129222859bed3a14e38
gnemoka <gnemo.ka@gmail.com>
2018-09-14 00:07:34 +0200

    Initial working version

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Opera = require('./lib/opera');
 const appId = 'd1ff258466994304854884d3600a75aa'
 const opera = Opera(appId);
 
-opera.latest()
+opera.currencies()
   .then( res => console.log(res))
   .catch(err => console.log(err));
 
